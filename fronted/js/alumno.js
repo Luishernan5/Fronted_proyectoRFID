f1c886a0114020877e0ee79001b001c1fc0ff8f8
Luishernan5 <ramirezluishernan5@gmail.com>
2025-05-16 02:16:56 -0600

    front actualizacion1

--- fronted/js/alumno.js
+++ fronted/js/alumno.js
@@ -1,15 +1,14 @@
-
 const user = JSON.parse(localStorage.getItem("usuario"));
 if (!user || user.rol !== "alumno") location.href = "login.html";
 
-document.getElementById("nombreUsuario").textContent = user.nombre;
-const lista = document.getElementById("listaMaterias");
-user.materias.forEach(materia => {
-  const li = document.createElement("li");
-  li.textContent = materia;
-  lista.appendChild(li);
-});
+const nombreEl = document.getElementById("nombreUsuario");
+if (nombreEl) nombreEl.textContent = user.nombre;
 
-function verPase() {
-  window.location.href = "ver_pase.html";
+const lista = document.getElementById("listaMaterias");
+if (lista && user.materias) {
+  user.materias.forEach(materia => {
+    const li = document.createElement("li");
+    li.textContent = materia;
+    lista.appendChild(li);
+  });
 }
